cc80d788517d3696b0a594078e426a671908fc7b
Evgeny Shevtsov <shevtsov@croc.one>
2019-11-26 03:10:26 +0300

    add: apple table migration

--- console/migrations/m191122_234800_create_apple_table.php
+++ console/migrations/m191122_234800_create_apple_table.php
@@ -9,6 +9,8 @@ class m191122_234800_create_apple_table extends \yii\db\Migration
             'id' => $this->primaryKey(),
             'color' => $this->string(18),
             'size' => $this->float()->defaultValue(1),
+            'top' => $this->string(),
+            'left' => $this->string(),
             'status' => $this->integer(1)->defaultValue(0),
             'fall_at' => $this->integer(),
             'created_at' => $this->integer(),
